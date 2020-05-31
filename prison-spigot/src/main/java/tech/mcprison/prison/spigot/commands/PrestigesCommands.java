d97db4535aff32e073a18bf1087305f9505a7429
gabrielecaretti <gabrycaretti@gmail.com>
2020-06-01 00:04:18 +0200

    I'll be, short
    Changelogs:
    - Added prestiges
    - You can enable them in the config.yml
    - They just works as prestiges now, independency's achieved... it needs the ranks module!
    - I feel, not good and confused

--- prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/PrestigesCommands.java
+++ prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/PrestigesCommands.java
@@ -1,9 +1,11 @@
 package tech.mcprison.prison.spigot.commands;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import tech.mcprison.prison.ranks.PrisonRanks;
 import tech.mcprison.prison.spigot.SpigotPrison;
 
 public class PrestigesCommands implements CommandExecutor {
@@ -11,41 +13,43 @@ public class PrestigesCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        // Check parameters
-        if (args.length == 0) {
+        if (SpigotPrison.getInstance().getConfig().getBoolean("prestiges")) {
 
-            if (sender.hasPermission("prison.admin")) {
-                sender.sendMessage(SpigotPrison.format("&8---------------------------"));
-                sender.sendMessage(SpigotPrison.format("     &3Prestiges commands:"));
-                sender.sendMessage(SpigotPrison.format("&8---------------------------"));
-                sender.sendMessage(SpigotPrison.format("&3/prestige  &8&l| &3Prestige, permission prison.prestige."));
-                sender.sendMessage(SpigotPrison.format("&3/prestiges  &8&l| &3Get a list of prestiges commands."));
-                sender.sendMessage(SpigotPrison.format("&3/prestiges template"));
-                sender.sendMessage(SpigotPrison.format("&8&l| &3Enable hacky prestiges."));
-
-            } else {
-                sender.sendMessage(SpigotPrison.format("&8---------------------------"));
-                sender.sendMessage(SpigotPrison.format("     &3Prestiges commands:"));
-                sender.sendMessage(SpigotPrison.format("&8---------------------------"));
-                sender.sendMessage(SpigotPrison.format("&3/prestige     &8&l| &3Prestige, you can execute it if you're at the last rank and there're prestiges in the server."));
+            if (!(PrisonRanks.getInstance().getLadderManager().getLadder("prestiges").isPresent())) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ranks ladder create prestiges");
             }
 
+            // Check parameters
+            if (args.length == 0) {
+
+                if (sender.hasPermission("prison.admin")) {
+                    sender.sendMessage(SpigotPrison.format("&8---------------------------"));
+                    sender.sendMessage(SpigotPrison.format("     &3Prestiges commands:"));
+                    sender.sendMessage(SpigotPrison.format("&8---------------------------"));
+                    sender.sendMessage(SpigotPrison.format("&3/prestige  &8&l| &3Prestige, permission prison.prestige."));
+                    sender.sendMessage(SpigotPrison.format("&3/prestiges  &8&l| &3Get a list of prestiges commands."));
+                    sender.sendMessage(SpigotPrison.format("&8&l| &3Enable hacky prestiges."));
+
+                } else {
+                    sender.sendMessage(SpigotPrison.format("&8---------------------------"));
+                    sender.sendMessage(SpigotPrison.format("     &3Prestiges commands:"));
+                    sender.sendMessage(SpigotPrison.format("&8---------------------------"));
+                    sender.sendMessage(SpigotPrison.format("&3/prestige     &8&l| &3Prestige, you can execute it if you're at the last rank and there're prestiges in the server."));
+                }
+
+                return true;
+            }
+
+            String param = args[0];
+            int NotNull = 0;
+            while (NotNull != args.length - 1) {
+                args[NotNull] = args[NotNull + 1];
+                NotNull++;
+            }
+
+            args = (String[]) ArrayUtils.removeElement(args, args[NotNull]);
             return true;
         }
-
-        String param = args[0];
-        int NotNull = 0;
-        while (NotNull != args.length - 1){
-            args[NotNull] = args[NotNull + 1];
-            NotNull++;
-        }
-
-        args = (String[]) ArrayUtils.removeElement(args, args[NotNull]);
-
-        if (param.equalsIgnoreCase("template")){
-            return PrestigesTemplateCommand.onCommand(sender);
-        }
-
         return true;
     }
 }
--- prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/PrestigesPrestigeCommand.java
+++ prison-spigot/src/main/java/tech/mcprison/prison/spigot/commands/PrestigesPrestigeCommand.java
@@ -5,7 +5,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import tech.mcprison.prison.Prison;
+import tech.mcprison.prison.modules.Module;
+import tech.mcprison.prison.modules.ModuleManager;
 import tech.mcprison.prison.ranks.PrisonRanks;
+import tech.mcprison.prison.ranks.managers.LadderManager;
 import tech.mcprison.prison.spigot.SpigotPrison;
 
 public class PrestigesPrestigeCommand implements CommandExecutor {
@@ -13,28 +17,51 @@ public class PrestigesPrestigeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (!(sender instanceof Player || sender instanceof tech.mcprison.prison.internal.Player)) {
-            sender.sendMessage(SpigotPrison.format("&cFor some reasons, it looks like you aren't a player"));
-            return true;
-        }
+        if (SpigotPrison.getInstance().getConfig().getBoolean("prestiges")) {
 
+            if (!(PrisonRanks.getInstance().getLadderManager().getLadder("prestiges").isPresent())) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ranks ladder create prestiges");
+            }
 
-        if (!(PrisonRanks.getInstance().getLadderManager().getLadder("default").isPresent()) ||
-                !(PrisonRanks.getInstance().getLadderManager().getLadder("default").get().getLowestRank().isPresent()) ||
-                PrisonRanks.getInstance().getLadderManager().getLadder("default").get().getLowestRank().get().name == null){
-            sender.sendMessage(SpigotPrison.format("&cThere aren't ranks in the default ladder"));
-            return true;
-        }
+            Player p = null;
 
-        if (!(PrisonRanks.getInstance().getLadderManager().getLadder("prestiges").isPresent()) ||
-                !(PrisonRanks.getInstance().getLadderManager().getLadder("prestiges").get().getLowestRank().isPresent()) ||
-                PrisonRanks.getInstance().getLadderManager().getLadder("prestiges").get().getLowestRank().get().name == null){
-            sender.sendMessage(SpigotPrison.format("&cThere aren't prestiges in the prestige ladder"));
-            return true;
-        }
+            if (!(sender instanceof Player || sender instanceof tech.mcprison.prison.internal.Player)) {
+                sender.sendMessage(SpigotPrison.format("&cFor some reasons, it looks like you aren't a player"));
+                return true;
+            } else {
+                if (sender instanceof Player) {
+                    p = (Player) sender;
+                }
+            }
 
-        if (sender.hasPermission("ranks.rankup.prestiges")){
+            PrisonRanks rankPlugin;
+
+            ModuleManager modMan = Prison.get().getModuleManager();
+            Module module = modMan == null ? null : modMan.getModule( PrisonRanks.MODULE_NAME ).orElse( null );
+
+            rankPlugin = (PrisonRanks) module;
+
+            LadderManager lm = null;
+            if (rankPlugin != null) {
+                lm = rankPlugin.getLadderManager();
+            }
+
+            if (lm != null && (!(lm.getLadder("default").isPresent()) ||
+                    !(lm.getLadder("default").get().getLowestRank().isPresent()) ||
+                    lm.getLadder("default").get().getLowestRank().get().name == null)) {
+                sender.sendMessage(SpigotPrison.format("&cThere aren't ranks in the default ladder"));
+                return true;
+            }
+
+            if (lm != null && (!(lm.getLadder("prestiges").isPresent()) ||
+                    !(lm.getLadder("prestiges").get().getLowestRank().isPresent()) ||
+                    lm.getLadder("prestiges").get().getLowestRank().get().name == null)) {
+                sender.sendMessage(SpigotPrison.format("&cThere aren't prestiges in the prestige ladder"));
+                return true;
+            }
+
             Bukkit.dispatchCommand(sender, "rankup prestiges");
+
         }
 
         return true;
